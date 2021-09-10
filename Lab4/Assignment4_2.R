1fc04af878c4c186f68ace564132757f2f328139
Divya-varma <88661066+Divya-varma@users.noreply.github.com>
2021-09-10 11:49:05 +0530

    Add files via upload

---
+++ Lab4/Assignment4_2.R
@@ -0,0 +1,11 @@
+#2a
+library(MASS)
+plot(density(Animals$body))
+logbody <- log(Animals$body)
+plot(density(logbody))
+
+#2b
+av <- mean(logbody)
+sdev <- sd(logbody)
+xval <- pretty(c(av - 3 * sdev, av + 3 * sdev), 50)
+lines(xval, dnorm(xval, mean = av, sd = sdev))
