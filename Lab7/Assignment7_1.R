c50c9ef77ef96cd17e2766aa959027712a3fec29
Divya-varma <88661066+Divya-varma@users.noreply.github.com>
2021-11-01 12:26:17 +0530

    Add files via upload

---
+++ Lab7/Assignment7_1.R
@@ -0,0 +1,22 @@
+#Import any database of transactions of your choice (Groceries' dataset may be preferred.
+#It is predefined in R package)
+#(a) Load required R libraries : arules and arulesViz
+
+# Load the libraries
+library(arules)
+library(arulesViz)
+library(datasets)
+
+# Load the data set
+data(Groceries)
+
+# Create an item frequency plot for the top 20 items
+itemFrequencyPlot(Groceries,topN=20,type="absolute")
+
+# Get the rules
+rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.8))
+
+# Show the top 5 rules, but only 2 digits
+options(digits=2)
+rules<-sort(rules, decreasing=TRUE,by="confidence")
+inspect(rules[1:20])
